fab2272afbd44e6914d54af3bac88231f2de1a36
jspenc72 <jspenc72@gmail.com>
2016-07-12 17:19:06 -0600

    Initial Commit with Yeoman Generator that creates a new page to a sidemenu style ionic application.

---
+++ generator-ionic2-pro/generators/app/index.js
@@ -0,0 +1,15 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // Next, add your custom code
+    this.option('coffee'); // This method adds support for a `--coffee` flag
+  },
+  info: function () {
+    console.log('Use one of the sub generators to create generate items for your project.');
+  } 
+});
---
+++ generator-ionic2-pro/generators/page/index.js
@@ -0,0 +1,80 @@
+var generators = require('yeoman-generator');
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // Next, add your custom code
+    this.option('coffee'); // This method adds support for a `--coffee` flag
+  },
+  //Ask for user input
+  prompting: function() {
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'What would you like to call your page?',
+      //Defaults to the project's folder name if the input is skipped
+      default: this.appname + "NewPage"
+    }, function(answers) {
+      this.props = answers
+      this.log(answers.name);
+
+      done();
+    }.bind(this));
+  },
+  //Writing Logic here
+  writing: {
+    //Copy the template files
+    htmlTemplate: function () {
+            this.fs.copyTpl(
+                this.templatePath('page.html'),
+                this.destinationPath('./app/pages/'+this.props.name.toLowerCase()+'/'+this.props.name+'.html'), {
+                    name: this.props.name,
+                    capitalized: capitalizeFirstLetter(this.props.name),
+                    uppercase: this.props.name.toUpperCase(),
+                    lowercase: this.props.name.toLowerCase()
+                }
+            );
+        },
+    scssTemplate: function () {
+            this.fs.copyTpl(
+                this.templatePath('page.scss'),
+                this.destinationPath('./app/pages/'+this.props.name.toLowerCase()+'/'+this.props.name+'.scss'), {
+                    name: this.props.name,
+                    capitalized: capitalizeFirstLetter(this.props.name),
+                    uppercase: this.props.name.toUpperCase(),
+                    lowercase: this.props.name.toLowerCase()
+                }
+            );
+        },
+    tsTemplate: function () {
+            this.fs.copyTpl(
+                this.templatePath('page.ts'),
+                this.destinationPath('./app/pages/'+this.props.name.toLowerCase()+'/'+this.props.name+'.ts'), {
+                    name: this.props.name,
+                    capitalized: capitalizeFirstLetter(this.props.name),
+                    uppercase: this.props.name.toUpperCase(),
+                    lowercase: this.props.name.toLowerCase()
+                }
+            );
+        },
+    jsTemplate: function () {
+            this.fs.copyTpl(
+                this.templatePath('page.js'),
+                this.destinationPath('./app/pages/'+this.props.name.toLowerCase()+'/'+this.props.name+'.js'), {
+                    name: this.props.name,
+                    capitalized: capitalizeFirstLetter(this.props.name),
+                    uppercase: this.props.name.toUpperCase(),
+                    lowercase: this.props.name.toLowerCase()
+                }
+            );
+        }
+  }
+});
